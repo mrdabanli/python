bea0c3eead9146233db0c1cb78075871d5f1f8ae
Hakan Dabanlı <mrdabanli@gmail.com>
2019-08-23 17:08:40 +0300

    some scripts updated.

--- scripts/bit-counter.py
+++ scripts/bit-counter.py
@@ -1,12 +1,12 @@
 bits = [] #list of bits
-count = int(input("how many bits do you want ?: ")) #1 byte
+count = int(input("How many bits do you want ?: ")) #number of bits
 
 for bit in range(count):
     print("")
-    bits.append(int(input("0/1 ?: ")))
+    bits.append(int(input("0/1 ?: "))) #append bits to list
 
-bitCounter = 0
-repeatedCount = 0
+bitCounter = 0 #count the number of 1's
+repeatedCount = 0 #count repeated 1's
 
 for bit in bits:
     if bit == 1:
